451cd6ebe1afe1a0fb852d23b40b7d853ec236eb
Paulo Pereira <phfaverop@gmail.com>
2024-01-11 00:41:05 -0300

    feat: :sparkles: main server launcher

---
+++ app/server/main.py
@@ -0,0 +1,45 @@
+"""FastAPI Server Setup"""
+from datetime import datetime
+from http import HTTPStatus
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from config import environment
+from .routes.users import user_router
+
+allowed_methods = ["POST, GET", "DELETE", "PUT", "PATCH"]
+
+tags_metadata = [
+    {
+        "name": "E-commerce API",
+        "description": "A set of endpoints to to register and manage e-commerce data",
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=allowed_origins,
+    allow_credentials=False,
+    allow_methods=allowed_methods,
+    allow_headers=["*"],
+)
+
+app.include_router(user_router)
+
+
+@app.get("/", status_code=HTTPStatus.OK)
+def root():
+    return "E-commerce Application API"
+
+
+@app.on_event("startup")
+def startup_event():
+    print(f"{datetime.now()} - Starting up server...")
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    print(f"{datetime.now()} - Shutting down server...")
